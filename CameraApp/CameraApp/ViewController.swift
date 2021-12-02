131836a3aec4136e6161524d79de0b4d57503018
Areej Mohammad <areejmohammad@Areejs-MacBook-Air.local>
2021-12-02 13:46:48 +0300

    adding xcode file

---
+++ CameraApp/CameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Areej Mohammad on 27/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
